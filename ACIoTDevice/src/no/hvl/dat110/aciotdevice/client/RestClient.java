d61a6e3f7ba34552a4a8bd38895acae43fc4fabe
Olav Wessel Pete <olav.pete@gmail.com>
2021-05-06 11:44:11 +0200

    Update RestClient.java

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,27 +1,54 @@
 package no.hvl.dat110.aciotdevice.client;
 
-public class RestClient {
+import com.google.gson.Gson;
+import no.hvl.dat110.ac.restservice.AccessEntry;
+import no.hvl.dat110.ac.restservice.AccessLog;
 
-	public RestClient() {
-		// TODO Auto-generated constructor stub
-	}
+import static spark.Spark.get;
+import static spark.Spark.post;
 
-	private static String logpath = "/accessdevice/log";
+public class RestClient
+{
+	private AccessLog accessLog;
 
-	public void doPostAccessEntry(String message) {
+    public RestClient()
+    {
+        // TODO Auto-generated constructor stub
+		accessLog = new AccessLog();
+    }
 
-		// TODO: implement a HTTP POST on the service to post the message
-		
-	}
-	
-	private static String codepath = "/accessdevice/code";
-	
-	public AccessCode doGetAccessCode() {
+    private static String logpath = "/accessdevice/log";
 
-		AccessCode code = null;
-		
-		// TODO: implement a HTTP GET on the service to get current access code
-		
-		return code;
-	}
+    public void doPostAccessEntry(String message)
+    {
+
+        // TODO: implement a HTTP POST on the service to post the message
+        post(logpath, (req, res) ->
+        {
+        	Gson gson = new Gson();
+
+			int id = accessLog.add(message);
+
+            return gson.toJson(accessLog.get(id));
+        });
+    }
+
+    private static String codepath = "/accessdevice/code";
+
+    public AccessCode doGetAccessCode()
+    {
+
+        AccessCode code = null;
+
+        // TODO: implement a HTTP GET on the service to get current access code
+        get(codepath, (req, res) ->
+        {
+            Gson gson = new Gson();
+
+
+            return "";
+        });
+
+        return code;
+    }
 }
